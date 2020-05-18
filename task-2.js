96ff255009b6074626bb0ba79e10a8931ca3f1c0
bohdankyrylenko <bakirilenko@gmail.com>
2020-05-18 18:32:14 +0300

    task1

---
+++ task-2.js
@@ -0,0 +1,21 @@
+//Организовать функцию getInfo, которая принимает объект вида
+//{ name: ..., info: { employees: [...], partners: [ … ] } }
+//и выводит в консоль имя (если имени нет, показывать ‘Unknown’) и первые две компании из массива partners:
+
+const organisation = {
+  name: "Google",
+  info: {
+    employees: ["Vlad", "Olga"],
+    partners: ["Microsoft", "Facebook", "Xing"],
+  },
+};
+
+const getInfo = (organisation) => {
+  const {
+    name,
+    info: { partners },
+  } = organisation;
+  return { name: name || "UNKNOWN", partners: partners.slice(0, 2) };
+};
+
+console.log(getInfo(organisation));
